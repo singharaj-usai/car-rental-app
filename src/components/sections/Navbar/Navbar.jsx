155c83929803eabdef35de5dff713ed3e4503969
singharaj usai <djjd22938@gmail.com>
2023-08-08 03:21:17 -0400

    vehicle models page

--- src/components/sections/Navbar/Navbar.jsx
+++ src/components/sections/Navbar/Navbar.jsx
@@ -3,6 +3,10 @@ import { HashRouter as Router, NavLink, Route, Switch } from "react-router-dom";
 import Home from "../../pages/About/Home";
 import VehicleModels from "../../pages/VehicleModels/VehicleModels";
 import Logo from "../../../assets/images/car-rental-logo.png";
+import Aventador from "../../../assets/images/2013-Aventador.jpg";
+import R from "../../../assets/images/red-aston-martin.jpg";
+import {FaPhone} from "react-icons/fa";
+import PhoneBanner from "../../pages/PhoneBanner/PhoneBanner";
 
 const navbarData = [
     {
@@ -85,7 +89,32 @@ const Navbar = () => {
                     <Home />
                 </Route>
                 <Route path="/vehiclemodels">
-                    <VehicleModels />
+                    <div className="w-full md:order-1" style={{ backgroundImage: `url(${Aventador})`, backgroundSize: "cover", backgroundPosition: "center 75%" }}>
+                        <div className="h-80 relative overflow-hidden">
+                            <div className="absolute inset-0 bg-opacity-75 bg-black">
+                                <div className="px-12 text-white pt-12">
+                                    <h1 className="text-4xl font-bold mb-3">Vehicle Models</h1>
+                                    <p className="text-lg font-semibold">
+                                        Home / Vehicle Models
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap md:px-6 p-2 mb-6">
+                        <div className="w-full leading-relaxed">
+                            <div className="w-full leading-relaxed">
+                                <VehicleModels/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap">
+                        <div className="w-full leading-relaxed">
+                            <div className="w-full leading-relaxed">
+                                <PhoneBanner/>
+                            </div>
+                        </div>
+                    </div>
                 </Route>
                 {/* Add your sign-in and register route components */}
                 <Route path="/signin">
