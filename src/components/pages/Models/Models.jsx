dc71548654a2af17aad28c3dc10f9f8128e0287d
singharaj usai <djjd22938@gmail.com>
2023-09-08 19:18:48 -0400

    5xl

--- src/components/pages/Models/Models.jsx
+++ src/components/pages/Models/Models.jsx
@@ -110,7 +110,10 @@ const VehicleModels = () => {
     return (
         <section className="px-4 py-12">
             <div className="text-center">
-                <h1 className="text-4xl text-gray-800 font-bold mb-4">
+                <h3 className="text-2xl text-gray-800 font-medium mb-4">
+                    Vehicle Models
+                </h3>
+                <h1 className="text-5xl text-gray-800 font-bold mb-4">
                     Our Rental Fleet
                 </h1>
                 <p className="text-gray-500 mb-8">
