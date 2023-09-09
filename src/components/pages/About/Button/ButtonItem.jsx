b08cf0df5b6fcefd93faec3c4636e553e58179d7
singharaj usai <djjd22938@gmail.com>
2023-09-08 22:46:25 -0400

    react-scroll for hero section

--- src/components/pages/About/Button/ButtonItem.jsx
+++ src/components/pages/About/Button/ButtonItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaCheckCircle  } from "react-icons/fa";
+import {Link} from "react-scroll";
 
 const ButtonItem = (props) => {
     const { text } = props.button;
@@ -7,12 +8,13 @@ const ButtonItem = (props) => {
         <div className="w-full">
             <div className="mb-4">
                 <div className="flex justify-between items-center mb-1">
-                    <button className="hover:text-red-500 hover:bg-gray-50 transition ease-out duration-200 text-md text-gray-50 font-bold bg-red-500 border-red-500 border-2 px-10 py-3 rounded flex items-center">
+                    <Link to="bookingSection" smooth={true} duration={500}
+                        className="hover:text-red-500 hover:bg-gray-50 transition ease-out duration-200 text-md text-gray-50 font-bold bg-red-500 border-red-500 border-2 px-10 py-3 rounded flex items-center">
                         {text}
                         <span className="ml-2">
                             <FaCheckCircle />
                         </span>
-                    </button>
+                    </Link>
                 </div>
             </div>
         </div>
